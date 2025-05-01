27f98e56f63044287c280fa3ad55d2ee51c78751
Jordanx777 <jordanovinasco777@gmail.com>
2025-04-30 22:39:13 -0500

    feat: enhance dashboard with form fields for user input and styling

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,13 +1,40 @@
 
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatCardModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatTimepickerModule,
+    MatSelectModule
+   
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  nombres: string = '';
+  apellidos: string = '';
+  meetingDate: Date | null = null;
+  meetingTime: string = '';
+  value: Date = new Date();
 }
