fcbe68163bdf22bb649a07b16bd02a94a322e00f
Luis H <luisinfotv@gmail.com>
2025-05-02 21:59:31 -0500

    feat: Refactor dashboard and add AgregarComisiones component
    - Updated app.component.html to include a new link for "Agregar comisiones".
    - Modified app.routes.ts to add a route for the AgregarComisionesComponent.
    - Enhanced dashboard.component.html and dashboard.component.ts to implement a sidebar navigation with dynamic view switching.
    - Added styles to dashboard.component.css for improved layout and responsiveness.
    - Refactored agregar-taxista.component.html and agregar-taxista.component.css for better input organization and styling.
    - Created AgregarComisionesComponent with basic structure and styles.
    - Added unit tests for AgregarComisionesComponent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './homeComponent/home/home.component';
 import { AgregarTaxistaComponent } from './modulo_taxistas/agregar-taxista/agregar-taxista.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AgregarComisionesComponent } from './modulo_comisiones/agregar-comisiones/agregar-comisiones.component';
 
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'agregar', component: AgregarTaxistaComponent},
-    {path: 'dashboard', component: DashboardComponent}, // Cambia el componente según sea necesario
+    {path: 'agregar comisiones', component: AgregarComisionesComponent},
+    {path: 'dashboard', component: DashboardComponent}, 
 ];
